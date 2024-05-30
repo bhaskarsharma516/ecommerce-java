22f9b003a86ae819ceea342d6723a7fcc527e073
Bhaskar sharma <bhaskarsharma516@gmail.com>
2024-05-30 13:16:21 +0530

    user registration

--- src/main/java/com/ecommerce/usermanagement/UserManagementApplication.java
+++ src/main/java/com/ecommerce/usermanagement/UserManagementApplication.java
@@ -1,13 +1,13 @@
-package com.ecommerce;
+package com.ecommerce.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class UserManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
+		SpringApplication.run(UserManagementApplication.class, args);
 	}
 
 }
--- src/test/java/com/ecommerce/usermanagement/UserManagementApplicationTests.java
+++ src/test/java/com/ecommerce/usermanagement/UserManagementApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ecommerce;
+package com.ecommerce.usermanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class UserManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
